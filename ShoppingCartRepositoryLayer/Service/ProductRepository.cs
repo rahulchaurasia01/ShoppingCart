b72d9b9a57dafee80fb1e73ef0eab9d6cee27df4
Rahul Chaurasia <rahulchaurasia92@hotmail.com>
2020-04-20 11:44:53 +0530

    Implemented Interfaces and Services for Product Controller.

---
+++ ShoppingCartRepositoryLayer/Service/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using ShoppingCartRepositoryLayer.Interface;
+using ShoppingCartRepositoryLayer.ModelContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartRepositoryLayer.Service
+{
+    public class ProductRepository : IProductRepository
+    {
+
+        private readonly ApplicationContext _applicationContext;
+
+        public ProductRepository(ApplicationContext applicationContext)
+        {
+            _applicationContext = applicationContext;
+        }
+
+    }
+}
