d7a5e46b95e72e3c9de76e5c20850a4995560057
Rahul Chaurasia <rahulchaurasia92@hotmail.com>
2020-04-20 11:40:26 +0530

    Added Model For Product Table in Db ShoppingCartDb

---
+++ ShoppingCartCommonLayer/ModelDb/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartCommonLayer.ModelDb
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductId { set; get; }
+
+        [Required]
+        public string Name { set; get; }
+
+        public string Image { set; get; }
+
+        [Required]
+        public int Quantity { set; get; }
+
+        [Required]
+        public int Price { set; get; }
+
+        public DateTime CreatedAt { set; get; }
+
+        public DateTime ModifiedAt { set; get; }
+
+    }
+}
