75b17ebc2499f2a7aadf06344ddecc8ac23571dd
Rahul Chaurasia <rahulchaurasia92@hotmail.com>
2020-04-20 11:44:15 +0530

    Add Migration for Creating Product Table in ShoppingCartDb

---
+++ ShoppingCartRepositoryLayer/Migrations/202004191402328_DbAndProductTableCreated.cs
@@ -0,0 +1,31 @@
+﻿namespace ShoppingCartRepositoryLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DbAndProductTableCreated : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Product",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Image = c.String(),
+                        Quantity = c.Int(nullable: false),
+                        Price = c.Int(nullable: false),
+                        CreatedAt = c.DateTime(nullable: false),
+                        ModifiedAt = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Product");
+        }
+    }
+}
---
+++ ShoppingCartRepositoryLayer/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace ShoppingCartRepositoryLayer.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ShoppingCartRepositoryLayer.ModelContext.ApplicationContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ShoppingCartRepositoryLayer.ModelContext.ApplicationContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
