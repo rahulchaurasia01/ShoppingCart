f035ab090e2164d75c4081bdd0b1cd1cc24c6504
Rahul Chaurasia <rahulchaurasia92@hotmail.com>
2020-04-20 11:34:13 +0530

    Created Business Layer for Shopping Cart.

---
+++ ShoppingCartBusinessLayer/Service/ProductBusiness.cs
@@ -0,0 +1,22 @@
+﻿using ShoppingCartBusinessLayer.Interface;
+using ShoppingCartRepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartBusinessLayer.Service
+{
+    public class ProductBusiness : IProductBusiness
+    {
+
+        private readonly IProductRepository _productRepository;
+
+        public ProductBusiness(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+    }
+}
