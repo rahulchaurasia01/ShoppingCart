e029f91cac491f967d475f9d4e07966b724b21d3
Rahul Chaurasia <rahulchaurasia92@hotmail.com>
2020-04-20 11:43:42 +0530

    Created Context for the Database

---
+++ ShoppingCartRepositoryLayer/ModelContext/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using ShoppingCartCommonLayer.ModelDb;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartRepositoryLayer.ModelContext
+{
+    public class ApplicationContext : DbContext
+    {
+
+        public ApplicationContext() : base("ShoppingCartDb")
+        {
+
+        }
+
+        public DbSet<Product> Products { set; get; }
+
+
+    }
+}
