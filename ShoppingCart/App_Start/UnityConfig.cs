712958ebf8aa64d9f8b4c66e259e1d5b47bd85c4
Rahul Chaurasia <rahulchaurasia92@hotmail.com>
2020-04-22 17:12:37 +0530

    Added Unity to Implement Dependency Injection

--- ShoppingCart/App_Start/UnityConfig.cs
+++ ShoppingCart/App_Start/UnityConfig.cs
@@ -1,3 +1,7 @@
+using ShoppingCartBusinessLayer.Interface;
+using ShoppingCartBusinessLayer.Service;
+using ShoppingCartRepositoryLayer.Interface;
+using ShoppingCartRepositoryLayer.Service;
 using System.Web.Mvc;
 using Unity;
 using Unity.Mvc5;
@@ -9,11 +13,14 @@ namespace ShoppingCart
         public static void RegisterComponents()
         {
 			var container = new UnityContainer();
-            
+
             // register all your components with the container here
             // it is NOT necessary to register your controllers
-            
+
             // e.g. container.RegisterType<ITestService, TestService>();
+
+            container.RegisterType<IProductBusiness, ProductBusiness>();
+            container.RegisterType<IProductRepository, ProductRepository>();
             
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
--- ShoppingCart/Global.asax.cs
+++ ShoppingCart/Global.asax.cs
@@ -16,6 +16,8 @@ namespace ShoppingCart
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            UnityConfig.RegisterComponents();
         }
     }
 }
