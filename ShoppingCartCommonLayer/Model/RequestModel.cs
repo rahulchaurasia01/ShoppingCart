0437f0ac1f8bdc51633efec56c102816f58d6456
Rahul Chaurasia <rahulchaurasia92@hotmail.com>
2020-04-22 17:15:07 +0530

    Create Request and Response Model For Adding Product

---
+++ ShoppingCartCommonLayer/Model/RequestModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartCommonLayer.Model
+{
+
+    /// <summary>
+    /// It's the Request Model For adding Product
+    /// </summary>
+    public class ProductRequestModel
+    {
+        [Required(ErrorMessage = "Product Name is Required.")]
+        public string Name { set; get; }
+
+        [Required(ErrorMessage = "Image Url Path Required.")]
+        public string Image { set; get; }
+
+        [Required(ErrorMessage = "Product Quantity is Required.")]
+        [RegularExpression("^[1-9][0-9]{0,3}$", ErrorMessage = "Min 1 and Max 9999 Quantity Of the Product Required.") ]
+        public int Quantity { set; get; }
+        
+        [Required(ErrorMessage = "Product Price is Required.")]
+        [RegularExpression("^[1-9][0-9]{0,6}$", ErrorMessage = "Price Of the Product Should be More than 0")]
+        public int Price { set; get; }
+
+    }
+
+
+}
---
+++ ShoppingCartCommonLayer/Model/ResponseModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartCommonLayer.Model
+{
+
+    /// <summary>
+    /// It's a response Model When Product is Added Successfully.
+    /// </summary>
+    public class ProductResponseModel
+    {
+
+        public int ProductId { set; get; }
+
+        public string Name { set; get; }
+
+        public string Image { set; get; }
+
+        public int Quantity { set; get; }
+
+        public int Price { set; get; }
+
+        public DateTime CreatedAt { set; get; }
+
+        public DateTime ModifiedAt { set; get; }
+
+    }
+
+}
