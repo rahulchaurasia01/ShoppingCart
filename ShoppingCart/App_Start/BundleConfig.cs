bb1ae5b5a18f6d285ceabc1ffe53886940b8e51e
Rahul Chaurasia <rahulchaurasia92@hotmail.com>
2020-04-29 18:43:15 +0530

    Added Font-awesome to bundling.

--- ShoppingCart/App_Start/BundleConfig.cs
+++ ShoppingCart/App_Start/BundleConfig.cs
@@ -29,6 +29,10 @@ namespace ShoppingCart
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/fontawesome").Include(
+                      "~/Content/font-awesome.css"));
+
         }
     }
 }
